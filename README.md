9d22d757907ee1528f3624190395e0776a5e9638
Davi RF <vitor25davi30@gmail.com>
2025-09-01 16:08:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# LETRIMAX
+
+**LETRIMAX** é um jogo de adivinhação de palavras inspirado no jogo Wordle, desenvolvido como trabalho para a matéria de Introdução à Programação de Computadores.
+
+## Descrição
+
+O objetivo do jogo é descobrir a **palavra secreta de 5 letras** em até **6 tentativas**. A cada tentativa, o jogo mostra quais letras estão corretas e em qual posição, utilizando cores para facilitar a visualização:
+
+- Letras **verdes**: estão na palavra e na posição correta
+- Letras **amarelas**: estão na palavra, mas em posição diferente
+- Letras **sem cor**: não estão na palavra
+
+## Como jogar
+
+1. Execute o programa Python que está no arquivo `codigo.py`
+2. Escolha a velocidade de digitação
+3. Digite palavras de 5 letras válidas até acertar a palavra ou acabarem suas tentativas
+5. Veja seu placar ao final de cada rodada
+
+## Requisitos
+
+- Python instalado
+- Biblioteca `unidecode`
+- Certifique-se de que o arquivo `br-utf8.txt` permanece na mesma pasta que o arquivo `código.py`
+
+## Autores
+
+- Davi Reis Furtado
+- Arthur Figueiredo Mozella
+- Gabriel de Almeida Dias
+
+## Referência
+
+FEOFILOFF, Paulo. br-utf8.txt – Lista de palavras do português brasileiro. São Paulo: Instituto de Matemática e Estatística da Universidade de São Paulo, 2025. [Disponível aqui](https://www.ime.usp.br/~pf/dicios/br-utf8.txt). Acesso em: 09 ago. 2025.
---
+++ codigo.py
@@ -0,0 +1,140 @@
+# importando as bibliotecas necessárias
+from time import sleep
+from unidecode import unidecode
+from random import choice
+
+# definições das funções que serão usadas
+TP = 0.05 # tempo de digitação padrão
+
+def digit(msg, fim=0, t=None):
+    if t is None: t = TP
+    for l in msg:
+        print(l, end='', flush=True)
+        sleep(t)
+    if fim == 1:
+        return input()
+    elif fim == 2: 
+        print(end='')
+    else: 
+        print()
+
+def val_resp(msg, conj_resp, erro_msg, fim=1, t=None, z=1):
+    resp = digit(msg, 1, t).strip().replace(' ', '').upper()
+    if z != 1:
+        resp = resp[0]
+    while resp not in conj_resp:
+        digit(erro_msg, fim)
+        resp = digit(msg, 1, t).strip().replace(' ', '').upper()
+        if z != 1:
+            resp = resp[0]
+    return resp
+
+# definição dos códigos de cor
+RESET = '\033[0;38;5;15m'
+BOLD = '\033[1;38;5;15m'
+GREEN = '\033[1;38;5;108m'
+YELLOW = '\033[1;38;5;220m'
+
+# introduzindo o jogador ao jogo
+digit(f'Seja bem vindo ao jogo {BOLD}LETRIMAX{RESET}, você terá {BOLD}6{RESET} chances para advinhar a {BOLD}PALAVRA SECRETA{RESET}, que tem {BOLD}5{RESET} letras. ', 1)
+digit(f'A cada palavra digitada você verá sua palavra novamente, mas algumas letras estarão pintadas.\n{GREEN}C A{RESET} {BOLD}U {YELLOW}S A{RESET}')
+digit(f'No exemplo anterior, o {BOLD}C{RESET} e o {BOLD}A{RESET} estão na palavra e na posição correta, pois estão na cor verde.\nAs letras {BOLD}S{RESET} e {BOLD}A{RESET} estão na posição errada, mas estão na palavra.\nE a letra {BOLD}U{RESET} não está na palavra. ', 1)
+digit(f'{GREEN}C A M A S{RESET}\nNesse caso, a palavra foi descoberta.')
+digit('Já vou avisando, parece simples, mas não é. ', 1)
+
+# escolha da velocidade de digitação
+digit('Mas antes de começar... ', 2)
+while True:
+    try:
+        ent = digit('Digite a velocidade da escrita em segundos (padrão = 0.05): ', 1).replace(' ', '').replace(',', '.')
+        if ent != '':
+            TP = float(ent)
+        if TP < 0:
+            TP = 0.05
+            digit('Erro! Sua entrada não pode ser menor que 0.')
+            raise ValueError
+        elif TP >= 1.5:
+            TP = 0.05
+            digit('Tá de sacanagem, né?! Escolha um valor menor que 1.5.')
+            raise ValueError
+        digit('Texto de exemplo')
+
+        conf = val_resp('Deseja manter nessa velocidade? [S/N] ', ['S', 'N'], 'Resposta inválida, tente novamente. ', t=0.05, z=0)
+        if conf == 'N':
+            continue
+        break
+    except Exception:
+        digit('Tente novamente. ', 2)
+digit('\nAperte enter para começar ', 1)
+digit('')
+
+# importando lista de todas as palavras com 5 letras da lingua portuguesa
+palavras = []
+try:
+    with open('br-utf8.txt', encoding='utf-8') as txt:
+        for linha in txt:
+            palavra = linha.strip().upper()
+            if len(palavra) == 5 and palavra.isalpha():
+                palavras.append(palavra)
+except Exception:
+    digit('Erro: verifique se o arquivo "br-utf8.txt" está na mesma pasta que o programa.')
+    exit()
+
+placar = {'Vitórias': 0, 'Derrotas': 0}
+while True:
+    if len(palavras) == 0:
+        digit('Infelizmente as palavras acabaram. ', 2)
+        break
+    pcerta = choice(palavras)
+    certa = unidecode(pcerta)
+    palavras.remove(pcerta)
+    m = 1
+    while m <= 6:
+        usu = unidecode(val_resp(f'{m}/6 - ', palavras, f'Palavra não identificada, tente novamente. ', 2))
+        resultado = [RESET] * 5
+        letras_usadas = [False] * 5
+        m += 1
+
+        # letras corretas em posições corretas
+        for i in range(5):
+            if usu[i] == certa[i]:
+                resultado[i] = GREEN
+                letras_usadas[i] = True
+        
+        # estão na palavra, mas em posições diferentes
+        for i in range(5):
+            if resultado[i] == GREEN: 
+                continue
+            for j in range(5):
+                if not letras_usadas[j] and usu[i] == certa[j]:
+                    resultado[i] = YELLOW
+                    letras_usadas[j] = True
+                    break
+        
+        # mostra a palavra corrogida
+        for i in range(5):
+            if i < 4:
+                print(resultado[i] + usu[i], end=' ')
+            else:
+                print(resultado[i] + usu[i], end=f'{RESET}\n', flush=True)
+            sleep(TP)
+        
+        if usu == certa:
+            placar['Vitórias'] += 1
+            digit(f'Você acertou a {BOLD}PALAVRA SECRETA{RESET}! {BOLD}{pcerta}{RESET}', 1)
+            break
+    else:
+        placar['Derrotas'] += 1
+        digit(f'Suas tentativas acabaram.\nA {BOLD}PALAVRA SECRETA{RESET} era {BOLD}{pcerta}{RESET} ', 1)
+    
+    mostrar_placar = val_resp('Quer ver seu placar? [S/N] ', ['S', 'N'], 'Resposta inválida, tente novamente. ', 2, z=0)
+    if mostrar_placar == 'S':
+        for key, value in placar.items():
+            digit(f'{key}: {value}')
+    print()
+
+    continuar = val_resp('Quer jogar mais uma vez? [S/N] ', ['S', 'N'], 'Resposta inválida, tente novamente. ', 2, z=0)
+    if continuar == 'N': 
+        break
+
+digit(f'\nObrigado por ter jogado {BOLD}LETRIMAX{RESET}!')
